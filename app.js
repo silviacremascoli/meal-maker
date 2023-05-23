a70195f361b4070c02ffb4054ff889051bd298b3
silviacremascoli <silcremascoli@gmail.com>
2023-05-23 16:08:12 +0200

    Added set method and get method

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 let menu = {
   _meal: "",
   _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (this._meal = mealToCheck);
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (this._price = priceToCheck);
+    }
+  },
+  get todaySpecial() {
+    if (this._meal && this._price) {
+      return `Today’s Special is ${this._meal} for $${this._price}!`;
+    } else {
+      return `Meal or price was not set correctly!`;
+    }
+  },
 };
+
+menu.meal = "Spaghetti al Pesto";
+menu.price = 14;
+
+console.log(menu);
+console.log(menu.todaySpecial);
